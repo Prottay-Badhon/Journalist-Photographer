09f04b54c4808617ddeb08c4ff0c0c98e374871f
Prottay-Badhon <prottaybadhon54@gmail.com>
2023-01-30 04:29:05 +0600

    show more button added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Service.css";
-
+// style={{backgroundImage: `url(${props.image})`}}
 const Service = (props) => {
+  const {id,name,price,description,image}=props.service;
+  console.log(image);
   return (
-    <div className="service" style={{backgroundImage: `url(${props.image})`}}>
+    <div className="service" style={{backgroundImage: `url(${image})`}}>
       <div className="service-container">
         <div className="ps-2">
-        <h3>DIAMOND HEART RING</h3>
-        <p>$99</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam id natus laudantium.</p>
+        <h3>{name}</h3>
+        <p>${price}</p>
+       
+        <p>{description}</p>
         </div>
         <Link to="/checkout">
         <button className="add-to-cart-btn">
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Service from '../Service/Service'
 import banner1 from "../../images/Banner/banner1.jpg";
 import newspapper from "../../images/Banner/newspaper.jpg"
 import city from "../../images/Banner/city.jpg";
 import village from "../../images/Banner/village.jpg"
 import "./Services.css"
+import { Link } from 'react-router-dom';
 const Services = () => {
+  const [showedServices,setShowedServices]=useState(4);
+  const [servicesData, setServices] = useState([]);
+  const [visible,setVisible]=useState(true);
+  const showMore=()=>{
+    setShowedServices(servicesData.length);
+    setVisible(false);
+  }
+  useEffect(() => {
+    fetch("servicesData.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
+    
     <div className='services'>
         <div className="services-info">
-            <h1>Shop</h1>
-            <h4>LIMITED EDITION MERCH AVAILABLE</h4>
-            <p>The copy warned the Little Blind Text, that where it came from it would have been rewritten.</p>
+            <h1>Our Services</h1>
+            <h4 className='text-uppercase'>We explores the visual story of an event with the help of photographs</h4>
+            <p> </p>
             <button className='btn btn-outline-light'>View shop</button>
         </div>
-        <div className="services-container">
-        <Service image={newspapper}></Service>
-        <Service image={banner1}></Service>
-        <Service image={city}></Service>
-        <Service image={village}></Service>
+                <div className="services-container">
+                  {
+                    servicesData.map((service,index)=> (index<showedServices)? <Service service={service} key={service.id}></Service> : "")
+                  }
+                    
+                  {
+                (servicesData.length>4) ? <button className='btn btn-success' 
+                onClick={showMore} style={visible?{ display: "block"}:{display: "none"}}>Show more</button> : ""
+                  }
+              </div>
+                 
         
-
-        </div>
     </div>
+    
+
   )
 }
 
